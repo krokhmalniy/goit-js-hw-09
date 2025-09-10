24da9daca15db1170953202687982831f3eeef1c
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-09-10 19:35:08 +0300

    стилі для модалки

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,34 @@
+let formData = {
+  email: '',
+  message: '',
+};
 
+const feedbackForm = document.querySelector('.feedback-form');
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  formData = JSON.parse(savedData);
+  feedbackForm.elements.email.value = formData.email || '';
+  feedbackForm.elements.message.value = formData.message || '';
+}
+
+feedbackForm.addEventListener('input', onInput => {
+  formData[onInput.target.name] = onInput.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+feedbackForm.addEventListener('submit', sub => {
+  sub.preventDefault();
+
+  formData.email = feedbackForm.elements.email.value;
+  formData.message = feedbackForm.elements.message.value.trim();
+
+  localStorage.removeItem('feedback-form-state');
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  feedbackForm.reset();
+});
