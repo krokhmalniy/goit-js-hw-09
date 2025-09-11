8d222c4c173b1bf00e87bdd870123848c40e030e
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-09-11 07:48:15 +0300

    стилі для сторінок

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,6 +3,25 @@ let formData = {
   message: '',
 };
 
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+email.addEventListener('focus', () => {
+  email.placeholder = 'Type area';
+});
+
+email.addEventListener('blur', () => {
+  email.placeholder = ''; 
+});
+
+message.addEventListener('focus', () => {
+  message.placeholder = 'Type area';
+});
+
+message.addEventListener('blur', () => {
+  message.placeholder = ''; 
+});
+
 const feedbackForm = document.querySelector('.feedback-form');
 const savedData = localStorage.getItem('feedback-form-state');
 if (savedData) {
